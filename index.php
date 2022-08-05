9cbc8bd32d68cec6f913b5c855fb46dd02c014bc
Begench Hemrakulyyev <72478205+bench-soft@users.noreply.github.com>
2022-08-05 16:19:21 +0300

    Site Similar To Twitter

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twitter</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" /><link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="frontend/css/style.css">
+</head>
+<body>
+    
+   <section class="main-page">
+     
+   <div class="left">
+
+    <div class="left-content">
+        <div>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <h3 class="left-content-heading">
+                Find Your Interest
+            </h3>
+        </div>
+        <div>
+             <i class="fa-solid fa-user-group"></i>
+            <h3 class="left-content-heading">
+               Explore what people are talking about
+            </h3>
+        </div>
+        <div>
+             <i class="fa-solid fa-comment"></i>
+            <h3 class="left-content-heading">
+                Join the people
+            </h3>
+        </div>
+    </div>   
+
+
+   </div>
+   <div class="right">
+    <div class="middle-content">
+    <i class="fa-brands fa-twitter"></i>
+    <h1>See what's happening in the world right now</h1>
+    <h4>Join Twitter Now</h4>
+    <a href="signUp.php" class="sign-up">Sign Up</a>
+    <a href="login.php" class="log-in">Log In</a>
+    </div>
+   </div>
+   <footer class="main-page-footer">
+    
+      <ul>
+        <li>
+            <a href="#">About</a>
+        </li>
+        <li>
+            <a href="#">Help Center</a>
+        </li>
+        <li>
+            <a href="#">Term of Service</a>
+        </li>
+        <li>
+            <a href="#">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="#">Cookie Policy</a>
+        </li>
+        <li>
+            <a href="#">Ads Info</a>
+        </li>
+        <li>
+            <a href="#">Blog</a>
+        </li>
+        <li>
+            <a href="#">Status</a>
+        </li>
+        <li>
+            <a href="#">Careers</a>
+        </li>
+        <li>
+            <a href="#">Brand Resources</a>
+        </li>
+        <li>
+            <a href="#">Advertising</a>
+        </li>
+        <li>
+            <a href="#">Marketing</a>
+        </li>
+        <li>
+            <a href="#">Twitter for Business</a>
+        </li>
+        <li>
+            <a href="#">Developers</a>
+        </li>
+        <li>
+            <a href="#">Directory</a>
+        </li>
+        <li>
+            <a href="#">Setting</a>
+        </li>
+        <li>
+            <a href="#">&copy; <?php echo date('Y');  ?> Twitter.ing</a>
+        </li>
+      </ul>
+   
+   </footer role="contentinfo">
+
+   </section>
+
+</body>
+</html>
